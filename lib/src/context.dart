798e446d5e4b6e561b70703588282f284db2f92d
smartwalle <smartwalle@gmail.com>
2022-06-01 09:24:56 +0800

    注释

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -22,15 +22,15 @@ abstract class Context {
   // 路由名称
   String get routeName => _uri.path;
 
-  // 路由名称参数
+  // 路由查询参数
   String get query => _uri.query;
 
-  // 请求名称，路由名称加路由名称参数组成
+  // 请求名称，路由名称加路由查询参数组成
   late String _requestName;
 
   String get requestName => _requestName;
 
-  // 路由名称参数
+  // 路由查询参数
   late QueryParam _queryParam;
 
   QueryParam get queryParam => _queryParam;
