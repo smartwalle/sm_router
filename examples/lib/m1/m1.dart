f473b511b2637c006e5982c34a195be1f06dc242
SmartWalle <smartwalle@gmail.com>
2022-06-04 21:53:25 +0800

    更新示例

--- examples/lib/m1/m1.dart
+++ examples/lib/m1/m1.dart
@@ -5,6 +5,7 @@ import 'package:sm_router/sm_router.dart';
 void main() {
   RouteCenter.handle("/", (ctx) => const M1View1());
   RouteCenter.handle("/m1/view2", (ctx) => const M1View2());
+  RouteCenter.handle("/m1/view3", (ctx) => const M1View3());
 
   runApp(const MainApp());
 }
@@ -36,7 +37,9 @@ class M1View1 extends StatelessWidget {
             TextButton(
               child: const Text("进入 /m1/view2"),
               onPressed: () {
-                RouteCenter.push("/m1/view2");
+                RouteCenter.push("/m1/view2").then((value) {
+                  print(value);
+                });
               },
             ),
           ],
@@ -61,7 +64,40 @@ class M1View2 extends StatelessWidget {
             TextButton(
               child: const Text("返回 /m1/view1"),
               onPressed: () {
-                RouteCenter.pop();
+                RouteCenter.pop("from view2");
+              },
+            ),
+            TextButton(
+              child: const Text("进入 /m1/view3"),
+              onPressed: () {
+                RouteCenter.push("/m1/view3").then((value) {
+                  print(value);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class M1View3 extends StatelessWidget {
+  const M1View3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("M1 View3"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              child: const Text("返回 /m1/view2"),
+              onPressed: () {
+                RouteCenter.pop("from view3");
               },
             ),
           ],
