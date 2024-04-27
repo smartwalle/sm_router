af9ac52a1eb155ecf9fe3394cc6c67ab7d325b33
SmartWalle <smartwalle@gmail.com>
2024-04-27 12:41:13 +0800

    update

--- lib/src/route_information_parser.dart
+++ lib/src/route_information_parser.dart
@@ -5,12 +5,12 @@ import 'package:flutter/widgets.dart';
 class KIRouteInformationParser extends RouteInformationParser<String> {
   @override
   Future<String> parseRouteInformation(RouteInformation routeInformation) {
-    String routeName = routeInformation.location ?? "/";
+    String routeName = routeInformation.uri.path;
     return SynchronousFuture(routeName);
   }
 
   @override
   RouteInformation restoreRouteInformation(String configuration) {
-    return RouteInformation(location: configuration);
+    return RouteInformation(uri: Uri.parse(configuration));
   }
 }
